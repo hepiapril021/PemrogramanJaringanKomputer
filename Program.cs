fa98e7886e627ce1ad36f9a16708c6a9f2a26494
hepiapril021 <105156958+hepiapril021@users.noreply.github.com>
2022-10-03 01:09:05 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EncryptionDecryptionUsingSymmetricKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var key = "b14ca5898a4e4133bbce2ea2315a1916";
+
+            Console.WriteLine("Please enter a string for encryption");
+            var pass = Console.ReadLine();
+            var encryptedString = AesOperation.EncryptString(key, pass);
+            Console.WriteLine($"encrypted string = {encryptedString}");
+
+            var decryptedString = AesOperation.DecryptString(key, encryptedString);
+            Console.WriteLine($"decrypted string = {decryptedString}");
+
+            Console.ReadKey();
+        }
+    }
+}
